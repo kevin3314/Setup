9ace4a590ef29b0737ac8e1f8091cc116e2c73f3
Akutagawa <pbwfh384@gmail.com>
2020-07-24 04:51:38 +0000

    Add init on gcp script

---
+++ init_gcp.sh
@@ -0,0 +1,23 @@
+# Install zsh
+sudo apt-get install -y zsh
+chsh -s /usr/bin/zsh
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install neovim by build
+git clone https://github.com/neovim/neovim.git
+cd neovim
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+cd ..
+
+git clone https://github.com/kevin3314/NeoVimConfig.git
+mkdir -p ~/.config
+cp -r NeoVimConfig ~/.config/nvim
+
+git clone https://github.com/kevin3314/Setup.git
+cd Setup
+chmod 775 dotfile.sh
+./dotfile.sh
+cd ..
