780cd6102fed5cfb0a332d11d7c641add70cc99a
Akutagawa <pbwfh384@gmail.com>
2020-07-24 07:27:09 +0000

    Add clone plugin

--- init_gcp.sh
+++ init_gcp.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y zsh
 chsh -s /usr/bin/zsh
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Install neovim by build
 git clone https://github.com/neovim/neovim.git
